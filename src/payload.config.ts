19a56cba07027aed20b3c58dac2c5824e0a38e47
Yazal Ulloa <yazalulloa@gmail.com>
2025-08-25 14:48:33 -0400

    Initial commit

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -29,6 +29,7 @@ export default buildConfig({
   db: sqliteAdapter({
     client: {
       url: process.env.DATABASE_URI || '',
+      authToken: process.env.DATABASE_AUTH_TOKEN || '',
     },
   }),
   sharp,
